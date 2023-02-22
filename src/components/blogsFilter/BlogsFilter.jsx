23b6532aaebee9933c76c529b38ee477f286d9b6
Gul Akberova <kbrovagul0@gmail.com>
2023-02-22 13:55:09 +0400

    homepage blog filter design

--- src/components/blogsFilter/BlogsFilter.jsx
+++ src/components/blogsFilter/BlogsFilter.jsx
@@ -3,6 +3,31 @@ import blogsFilter from './blogsfilter.module.css'
 function BlogsFilter() {
   return (
     <section className={blogsFilter.blogsFilter_big_div}>
+      <h3>Discover more of what matters to you</h3>
+      <div className={blogsFilter.blogsFilter_mini_div}>
+        <a>Programming</a>
+        <a>Data Science</a>
+        <a>Programming</a>
+        <a>Prog</a>
+        <a>Programming</a>
+        <a>Prog</a>
+        <a>Data Science</a>
+        <a>Prog</a>
+
+      </div>
+      <div className={blogsFilter.blogsFilter_mini_div_help}>
+        <a>Help</a>
+        <a>Status</a>
+        <a>Help</a>
+        <a>Writers</a>
+        <a>Status</a>
+        <a>Help</a>
+        <a>Writers</a>
+        <a>Status</a>
+
+        <a>Help</a>
+      </div>
+
 
     </section>
   )
