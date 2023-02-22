ca1228083f0b6ea290d17e7b8bf694fad24d9e72
Gul Akberova <kbrovagul0@gmail.com>
2023-02-22 11:58:01 +0400

    home page trend components design

---
+++ src/components/trend/Trend.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import trend  from './trend.module.css'
+import trend1 from '../../assets/images/trend1.jpeg'
+
+function Trend() {
+  return (
+    <section className={trend.trend_big_div}>
+        <h3>Trending on Medium</h3>
+        <div className={trend.trend_mini_div}>
+            <div className={trend.trend_div}>
+                <h1>01</h1>
+                <div className={trend.trend_div_text}>
+                    <div  className={trend.trend_div_img}><img src={trend1}/><span>Praveen Seshadri</span></div>
+                    <h2>The maze is in the mouse</h2>
+                    <div  className={trend.trend_div_read}>
+                <span>15 Feb</span>.&nbsp; <span>4min read</span>
+                </div>
+                </div>
+                
+
+            </div>
+            <div className={trend.trend_div}>
+                <h1>01</h1>
+                <div className={trend.trend_div_text}>
+                    <div  className={trend.trend_div_img}><img src={trend1}/><span>Praveen Seshadri</span></div>
+                    <h2>The maze is in the mouse</h2>
+                    <div  className={trend.trend_div_read}>
+                <span>15 Feb</span>.&nbsp; <span>4min read</span>
+                </div>
+                </div>
+                
+
+            </div>
+            <div className={trend.trend_div}>
+                <h1>01</h1>
+                <div className={trend.trend_div_text}>
+                    <div  className={trend.trend_div_img}><img src={trend1}/><span>Praveen Seshadri</span></div>
+                    <h2>The maze is in the mouse</h2>
+                    <div  className={trend.trend_div_read}>
+                <span>15 Feb</span>.&nbsp; <span>4min read</span>
+                </div>
+                </div>
+                
+
+            </div>
+            <div className={trend.trend_div}>
+                <h1>01</h1>
+                <div className={trend.trend_div_text}>
+                    <div  className={trend.trend_div_img}><img src={trend1}/><span>Praveen Seshadri</span></div>
+                    <h2>The maze is in the mouse</h2>
+                    <div  className={trend.trend_div_read}>
+                <span>15 Feb</span>.&nbsp; <span>4min read</span>
+                </div>
+                </div>
+                
+
+            </div>
+            <div className={trend.trend_div}>
+                <h1>01</h1>
+                <div className={trend.trend_div_text}>
+                    <div  className={trend.trend_div_img}><img src={trend1}/><span>Praveen Seshadri</span></div>
+                    <h2>The maze is in the mouse</h2>
+                    <div  className={trend.trend_div_read}>
+                <span>15 Feb</span>.&nbsp; <span>4min read</span>
+                </div>
+                </div>
+                
+
+            </div>
+            <div className={trend.trend_div}>
+                <h1>01</h1>
+                <div className={trend.trend_div_text}>
+                    <div  className={trend.trend_div_img}><img src={trend1}/><span>Praveen Seshadri</span></div>
+                    <h2>The maze is in the mouse</h2>
+                    <div  className={trend.trend_div_read}>
+                <span>15 Feb</span>.&nbsp; <span>4min read</span>
+                </div>
+                </div>
+                
+
+            </div>
+
+        </div>
+
+
+    </section>
+  )
+}
+
+export default Trend
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
+import Trend from "../../components/trend/Trend";
 
 function HomePage() {
   return (
     <>
       <Hero />
+      <Trend/>
     </>
   );
 }
