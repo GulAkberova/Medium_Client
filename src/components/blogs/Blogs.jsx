38ed82e509d8b01fa8549b004db10c48b722dd04
Gul Akberova <kbrovagul0@gmail.com>
2023-02-22 15:29:22 +0400

    home page blog filter position stick

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -65,6 +65,86 @@ function Blogs() {
           <img src={blogs1} />
         </div>
       </div>
+      <div className={blogs.blogs_mini_div}>
+        <div className={blogs.blogs_mini_text}>
+          <div className={blogs.blogs_mini_text_img}>
+            <img src={trend1} />
+            <span>Praveen Seshadri</span>
+          </div>
+          <h2>The maze is in the mouse</h2>
+          <p>What ails Google. And how it can turn things around.</p>
+          <div className={blogs.blogs_mini_text_read}>
+            <div>
+            <span>15 Feb</span>.&nbsp; <span>4min read</span>
+            <button>Google</button>
+            </div>
+            <i class="fa-regular fa-bookmark"></i>
+          </div>
+        </div>
+        <div className={blogs.blogs_mini_img}>
+          <img src={blogs1} />
+        </div>
+      </div>
+      <div className={blogs.blogs_mini_div}>
+        <div className={blogs.blogs_mini_text}>
+          <div className={blogs.blogs_mini_text_img}>
+            <img src={trend1} />
+            <span>Praveen Seshadri</span>
+          </div>
+          <h2>The maze is in the mouse</h2>
+          <p>What ails Google. And how it can turn things around.</p>
+          <div className={blogs.blogs_mini_text_read}>
+            <div>
+            <span>15 Feb</span>.&nbsp; <span>4min read</span>
+            <button>Google</button>
+            </div>
+            <i class="fa-regular fa-bookmark"></i>
+          </div>
+        </div>
+        <div className={blogs.blogs_mini_img}>
+          <img src={blogs1} />
+        </div>
+      </div>
+      <div className={blogs.blogs_mini_div}>
+        <div className={blogs.blogs_mini_text}>
+          <div className={blogs.blogs_mini_text_img}>
+            <img src={trend1} />
+            <span>Praveen Seshadri</span>
+          </div>
+          <h2>The maze is in the mouse</h2>
+          <p>What ails Google. And how it can turn things around.</p>
+          <div className={blogs.blogs_mini_text_read}>
+            <div>
+            <span>15 Feb</span>.&nbsp; <span>4min read</span>
+            <button>Google</button>
+            </div>
+            <i class="fa-regular fa-bookmark"></i>
+          </div>
+        </div>
+        <div className={blogs.blogs_mini_img}>
+          <img src={blogs1} />
+        </div>
+      </div>
+      <div className={blogs.blogs_mini_div}>
+        <div className={blogs.blogs_mini_text}>
+          <div className={blogs.blogs_mini_text_img}>
+            <img src={trend1} />
+            <span>Praveen Seshadri</span>
+          </div>
+          <h2>The maze is in the mouse</h2>
+          <p>What ails Google. And how it can turn things around.</p>
+          <div className={blogs.blogs_mini_text_read}>
+            <div>
+            <span>15 Feb</span>.&nbsp; <span>4min read</span>
+            <button>Google</button>
+            </div>
+            <i class="fa-regular fa-bookmark"></i>
+          </div>
+        </div>
+        <div className={blogs.blogs_mini_img}>
+          <img src={blogs1} />
+        </div>
+      </div>
     </section>
   );
 }
