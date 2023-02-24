a348fd260b64ce63c2a18284ca819310f1aa1680
Gul Akberova <kbrovagul0@gmail.com>
2023-02-24 16:11:34 +0400

    user home page dropdown

--- src/components/menuItem/MenuDropdown.jsx
+++ src/components/menuItem/MenuDropdown.jsx
@@ -1,6 +1,4 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import Login from '../../pages/loginpage/Login';
+import * as React from 'react';
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
@@ -13,51 +11,22 @@ import Tooltip from '@mui/material/Tooltip';
 import PersonAdd from '@mui/icons-material/PersonAdd';
 import Settings from '@mui/icons-material/Settings';
 import Logout from '@mui/icons-material/Logout';
-import nav from './nav.module.css'
-import MenuDropdown from '../menuItem/MenuDropdown';
-function Header() {
-  const [open1, setOpen1] = React.useState(false);
-  const handleOpen = () => setOpen1(true);
 
-  // _______________________________________
-  const [colorChange, setColorchange] = useState(false);
-  const [color, changeColor] = useState("#FFC017");
-  const changeNavbarColor = () =>{
-    if(window.scrollY >= 495){
-      setColorchange(true);
-    }
-    else{
-      setColorchange(false);
-    }
- };
- window.addEventListener('scroll', changeNavbarColor);
-//  _________________________________________
-
-const [anchorEl, setAnchorEl] = React.useState(null);
-const open = Boolean(anchorEl);
-const handleClick = (event) => {
-  setAnchorEl(event.currentTarget);
-};
-const handleClose = () => {
-  setAnchorEl(null);
-};
-
+export default function MenuDropdown() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <>
-    <header style={{ background: color }} className={colorChange ? `${nav.nav_big_div} ${nav.nav_Color}` :  `${nav.nav_big_div}`}>
-        <h1><NavLink to={'/'} >MEDIUM</NavLink></h1>
-        <div>
-            <ul>
-                <li><NavLink >Our Story</NavLink></li>
-                <li><NavLink to='/member' >Membership</NavLink></li>
-                <li><NavLink>Write</NavLink></li>
-                <li><NavLink onClick={handleOpen}>Sign in</NavLink></li>
-
-            </ul>
-            <button className={nav.btn1}>
-                <NavLink >Get started</NavLink>
-            </button>
-            <Tooltip title="Account settings">
+    <React.Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Typography sx={{ minWidth: 100 }}>Contact</Typography>
+        <Typography sx={{ minWidth: 100 }}>Profile</Typography>
+        <Tooltip title="Account settings">
           <IconButton
             onClick={handleClick}
             size="small"
@@ -66,10 +35,11 @@ const handleClose = () => {
             aria-haspopup="true"
             aria-expanded={open ? 'true' : undefined}
           >
-            <Avatar sx={{ width: 32, height: 32 }}>G</Avatar>
+            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
           </IconButton>
         </Tooltip>
-        <Menu
+      </Box>
+      <Menu
         anchorEl={anchorEl}
         id="account-menu"
         open={open}
@@ -130,13 +100,6 @@ const handleClose = () => {
           Logout
         </MenuItem>
       </Menu>
-        </div>
-    </header>
-    <Login open={open1} setOpen={setOpen1}/>
-
-    
-    </>
-  )
+    </React.Fragment>
+  );
 }
-
-export default Header
