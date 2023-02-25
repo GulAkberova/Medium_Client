f351f3f4ba5a2dab93f7b133799ce0ee327abd0c
Gul Akberova <kbrovagul0@gmail.com>
2023-02-25 16:53:44 +0400

    blogs detail writer components design

--- src/components/blogsWriter/BlogsWriter.jsx
+++ src/components/blogsWriter/BlogsWriter.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import trend1 from '../../assets/images/trend1.jpeg'
+import detail from '../../pages/blogdetailpage/blogdetail.module.css'
 
 function BlogsWriter() {
   return (
-    <div>BlogsWriter</div>
+    <>
+    <div className={detail.blogs_writer_bigdiv}>
+      <img src={trend1}/>
+      <h3>FlowMapp</h3>
+      <span>9.4k Followers</span>
+      <p>Design exceptional UX for beautiful websites and products with online collaborative tools</p>
+      <div>
+        <button className={detail.blogs_writer_btn1}>Follow</button>
+        <button className={detail.blogs_writer_btn2}>Mail</button>
+      </div>
+
+    </div>
+    </>
   )
 }
 
